a8cf7e1511b789c27214b096adb4d14830f03b4e
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-27 08:09:50 +0530

    update

--- client/src/pages/Jobs.jsx
+++ client/src/pages/Jobs.jsx
@@ -5,6 +5,7 @@ import { Briefcase, MapPin, IndianRupee, Loader2 } from "lucide-react";
 
 export default function Jobs() {
   const [jobs, setJobs] = useState([]);
+  
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
   const navigate = useNavigate();
