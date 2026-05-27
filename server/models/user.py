98980aa36b920a4fdc9f7163fb4e0ca451bca8c9
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-27 08:11:25 +0530

    update

--- server/models/user.py
+++ server/models/user.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel, EmailStr
 
-class UserRegister(BaseModel):
+class RegisterUser(BaseModel):
     name: str
     email: EmailStr
     password: str
 
-class UserLogin(BaseModel):
+class LoginUser(BaseModel):
     email: EmailStr
     password: str
