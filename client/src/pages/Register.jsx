c1eeff23aa3a9b1b3269be0692cac07adbcb210c
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-28 16:44:48 +0530

    update

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -27,7 +27,7 @@ export default function Register() {
 
     try {
       await axios.post(
-        `${import.meta.env.VITE_API_URL}/auth/register`,
+        `${import.meta.env.VITE_API_URL}auth/register`,
         form
       );
 
