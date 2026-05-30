722ed776764fee6f819e346cad9810a1c77e0d85
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-30 17:28:24 +0530

    fix bugs

--- server/database/db.py
+++ server/database/db.py
@@ -6,10 +6,11 @@ load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
 
+print("MONGO_URL:", MONGO_URL)  # 👈 add this
+
 if not MONGO_URL:
     raise ValueError("MONGO_URL is not set in .env")
 
-# ✅ serverSelectionTimeoutMS add karo — fast fail, clear error
 client = MongoClient(MONGO_URL, serverSelectionTimeoutMS=5000)
 db = client["careerpilot"]
 users_collection = db["users"]
